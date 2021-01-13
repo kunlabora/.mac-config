e72539ac62a87e81a678990e3b781033ccc6ebbf
Sander Paulus <sander@48tzyz1.agso.local>
2021-01-13 08:40:12 +0100

    added digipolis profile

---
+++ profiles/digipolis/profile.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Load base profile
+source "$(dirname $0)/../base/profile.sh"
+
+# Configuration
+## General
+export DIGIPOLIS_PROFILE_HOME="$PROFILES_HOME/digipolis"
+export DIGIPOLIS_PROFILE="$DIGIPOLIS_PROFILE_HOME/profile.sh"
+export CURRENT_PROFILE="$DIGIPOLIS_PROFILE"
+
+## User
+export GIT_USER_NAME="SPaulus"
+export GIT_USER_EMAIL="sander.paulus@digipolis.be"
+
+# Execution
+## Git
+alias git="git -c user.name=$GIT_USER_NAME -c user.email=$GIT_USER_EMAIL"
